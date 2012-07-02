821630dd77b142cd5fa9e0b926010ac64df54d60
Picasso250 <cumt.xiaochi@gmail.com>
2012-07-03 00:13:17 +0800

    checked by JSLint

--- jquery.form_hold.dev.js
+++ jquery.form_hold.dev.js
@@ -3,33 +3,46 @@
  * using JSON so IE8+
  * using localStorage so IE10+, that's unecceptable! 或者说这个功能属于附属的奖励功能？
  * 每个页面有自己的不同的数据
+ * 这是DEV版，是最新版，是激进版，可能包含若干BUG
+ *
  * by: Ryan
- * version: 0.0.1
+ * version: 0.0.1b
  */
 
-(function ($) {
-    if (JSON===undefined || window.localStorage ===undefined) return;
+/*properties
+    bind, change, checkboxes, checkboxs, click, each, find, fn, is, join,
+    localStorage, location, others, parents, parse, pathname, prop, radios,
+    restoreFrom, saveAs, split, stringify, val
+*/
+
+(function ($, window) {
+    'use strict';
+    if (JSON === undefined || window.localStorage === undefined) {
+        return;
+    }
     var getName = function (form) {
         return form.prop('name') || form.prop('id') || form.find('form').prop('name') || 'unique';
     };
     var getLocalPath = function (name) {
-        return ['form_hold',location.pathname,name].join('.');
+        return ['form_hold', window.location.pathname, name].join('.');
     };
     var getData = function (form) {
-        var data = {radios:{},checkboxes:{},others:{}};
+        var data = {radios: {}, checkboxes: {}, others: {}};
         form.find('input,textarea,select').each(function () {
             var name = $(this).prop('name');
             switch ($(this).prop('type')) {
-                case 'radio':
-                    if ($(this).is(':checked')) data.radios[name] = $(this).val();
-                    break;
-                case 'checkbox':
-                    data.checkboxes[name] = $(this).prop('checked');
-                    break;
-                case 'email':
-                case 'text':
-                    data.others[name] = $(this).val();
-                    break;
+            case 'radio':
+                if ($(this).is(':checked')) {
+                    data.radios[name] = $(this).val();
+                }
+                break;
+            case 'checkbox':
+                data.checkboxes[name] = $(this).prop('checked');
+                break;
+            case 'email':
+            case 'text':
+                data.others[name] = $(this).val();
+                break;
             }
         });
         return data;
@@ -45,29 +58,43 @@
     };
     var readLocal = function (path) {
         var pathArr = path.split('.');
-        var allData = JSON.parse(window.localStorage[pathArr[0]]) || {};
+        var allData = JSON.parse(window.localStorage[pathArr[0]] || null) || {};
         var pageData = allData[pathArr[1]] || {};
         return pageData[pathArr[2]] || {};
     };
     var fillForm = function (form, data) {
         var others = data.others || {};
-        for (i in others) { // 不要忘记触发change()事件哦
-            form.find('input[name='+i+']:not([disabled])').val(others[i]).change();
-            form.find('textarea[name='+i+']:not([disabled])').val(others[i]).change();
-        }
+        $.each(others, function (i, x) {
+            form.find('input[name=' + i + ']:not([disabled])').val(x).change();
+            form.find('textarea[name=' + i + ']:not([disabled])').val(x).change();
+        });
+//        for (i in others) { // 不要忘记触发change()事件哦
+//            form.find('input[name=' + i + ']:not([disabled])').val(others[i]).change();
+//            form.find('textarea[name=' + i + ']:not([disabled])').val(others[i]).change();
+//        }
 
         var checkboxs = data.checkboxs || {};
-        for (i in checkboxs) {
-            var c = form.find('input[type=checkbox][name='+i+']:not([disabled])');
-            if (checkboxs[i] ^ c.prop('checked')) { // 如果两个不同，就要单击一下
+        $.each(checkboxs, function (i, chk) {
+            var c = form.find('input[type=checkbox][name=' + i + ']:not([disabled])');
+            if (chk !== c.prop('checked')) { // 如果两个不同，就要单击一下
                 c.click();
             }
-        }
+        });
+//        for (i in checkboxs) {
+//            var c = form.find('input[type=checkbox][name=' + i + ']:not([disabled])');
+//            if (checkboxs[i] ^ c.prop('checked')) { // 如果两个不同，就要单击一下
+//                c.click();
+//            }
+//        }
 
         var radios = data.radios || {};
-        for (i in radios) {
-            form.find('input[name='+i+'][type=radio][value='+radios[i]+']:not([disabled])').click();
-        }
+        $.each(radios, function (i, rd) {
+            form.find('input[name=' + i + '][type=radio][value=' + rd + ']:not([disabled])').click();
+        });
+//        for (i in radios) {
+//            form.find('input[name=' + i + '][type=radio][value=' + radios[i] + ']:not([disabled])').click();
+//        }
+
     };
     $.fn.saveAs = function (name) {
         var form = $(this);
@@ -90,4 +117,4 @@
             $(this).parents('form').saveAs(); // 这里的效率！！！ 似乎可以pushValueOfInput(input); save();
         });
     });
-})(jQuery);
+})(jQuery, window);
